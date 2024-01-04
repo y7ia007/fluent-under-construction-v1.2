db5dc5e561632a31d3e1b8183a51156f096b42fd
y7ia007 <y7ia007@gmail.com>
2024-01-04 13:59:11 +0200

    messaging

---
+++ src/components/messagesContainer/MessagesContainer.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './messagesContainer.css';
+
+function MessagesContainer() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    setNewMessage(e.target.value);
+  };
+
+  const handleSendClick = () => {
+    if (newMessage.trim() !== '') {
+      setMessages([...messages, { text: newMessage, id: messages.length }]);
+      setNewMessage('');
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="messages-container">
+        {messages.map((message) => (
+          <div key={message.id} className="message">
+            {message.text}
+          </div>
+        ))}
+      </div>
+      <div className="input-container">
+        <input
+          type="text"
+          value={newMessage}
+          onChange={handleInputChange}
+          placeholder="Type your message..."
+        />
+        <button onClick={handleSendClick}>Send</button>
+      </div>
+    </div>
+  );
+}
+
+export default MessagesContainer;
--- src/pages/Conversations.js
+++ src/pages/Conversations.js
@@ -2,6 +2,7 @@ import React from 'react'
 import MainSidebar from '../components/MainSidebar'
 import ChannelsSidebar from '../components/ChannelsSidebar'
 import MessagesSidebar from '../components/messagesSidebar/MessagesSidebar';
+import MessagesContainer from '../components/messagesContainer/MessagesContainer';
 
 const Conversations = () => {
     return (
@@ -9,6 +10,7 @@ const Conversations = () => {
             <MainSidebar />
             <ChannelsSidebar />
             <MessagesSidebar />
+            <MessagesContainer />
         </div>
     );
 }
